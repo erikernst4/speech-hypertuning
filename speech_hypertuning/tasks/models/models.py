41dd473c15e170e83f6b6e958a9f0e656667e348
Erik <erik.ernst@muttdata.ai>
2024-03-01 12:17:41 -0300

    Init notebooks

--- speech_hypertuning/tasks/models/models.py
+++ speech_hypertuning/tasks/models/models.py
@@ -1,5 +1,17 @@
+from typing import Any
 import pytorch_lightning as pl
+from torch import nn
+from s3prl.nn import S3PRLUpstream
 
 
 class WavLMBasePlusForSpeakerIdentification(pl.LightningModule):
-    pass
+    def __init__(
+        self,
+        downstream_size: int,
+    ) -> None:
+        super().__init__()
+        self.upstream = S3PRLUpstream("wavlm_base_plus")
+        self.downstream = nn.Linear(downstream_size)
+
+    def forward(self, x):
+        return self.downstream(self.upstream(x))
