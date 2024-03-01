5abf09afcc85d21405975217165b902593c360ea
Erik <erik.ernst@muttdata.ai>
2024-03-01 11:03:39 -0300

    fix: read voxceleb1

--- speech_hypertuning/scripts/run.sh
+++ speech_hypertuning/scripts/run.sh
@@ -1,5 +1,6 @@
 ginpipe configs/default_parameters.gin \
 			configs/train.gin \
+			configs/datasets/voxceleb1.gin \
 			--module_list configs/imports \
 			--project_name experiment_lr \
 			--experiment_name test_load_dataset
