71a24a40a8827b20ca63ee02d7544906d9266a69
Erik Ernst <erik_ernst@hotmail.com.ar>
2024-03-21 18:53:53 -0300

    fix: add ProcessorLoadUpstreamEmbedding

--- speech_hypertuning/tasks/load_data/__init__.py
+++ speech_hypertuning/tasks/load_data/__init__.py
@@ -3,4 +3,4 @@ from .load import load_dataset, read_audiodir, subsample_dataset
 from .process import (compensate_lengths, dataset_random_split,
                       dynamic_pad_batch, get_dataloaders, process_classes,
                       remove_long_audios)
-from .processor import ProcessorReadAudio
+from .processor import ProcessorReadAudio, ProcessorLoadUpstreamEmbedding
