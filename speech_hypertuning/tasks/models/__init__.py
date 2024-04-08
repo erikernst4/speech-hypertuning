9f4ab771ba6af6a5a9da14c25d9a5dc8cad52090
Erik <erik.ernst@muttdata.ai>
2024-04-08 19:16:10 -0300

    feat: add trainer test task

--- speech_hypertuning/tasks/models/__init__.py
+++ speech_hypertuning/tasks/models/__init__.py
@@ -2,3 +2,4 @@ from .extract import save_upstream_embeddings
 from .fit import fit_model
 from .load import load_model
 from .models import S3PRLUpstreamMLPDownstreamForCls
+from .test import test_model
