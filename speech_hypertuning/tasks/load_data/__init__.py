7d5aace07064363aa5a06fba45bba9c849e2e64a
Erik Ernst <erik_ernst@hotmail.com.ar>
2024-04-27 21:13:54 -0300

    fix: export create_splits

--- speech_hypertuning/tasks/load_data/__init__.py
+++ speech_hypertuning/tasks/load_data/__init__.py
@@ -2,5 +2,5 @@ from .dataloaders import DictDataset
 from .load import load_dataset, read_audiodir, subsample_dataset
 from .process import (compensate_lengths, dataset_random_split,
                       dynamic_pad_batch, get_dataloaders, process_classes,
-                      remove_long_audios)
+                      remove_long_audios, create_splits)
 from .processor import ProcessorReadAudio, ProcessorLoadUpstreamEmbedding
