86e1f73678d646f9dab9e3b7ae0266deaa884fd7
Erik Ernst <erik_ernst@hotmail.com.ar>
2024-04-28 01:42:30 -0300

    feat: full experiment parameters

--- speech_hypertuning/scripts/batch_size_vs_learning_rate.sh
+++ speech_hypertuning/scripts/batch_size_vs_learning_rate.sh
@@ -1,7 +1,7 @@
 
-for lr in 1 0.5 0.1 0.05 0.01 0.005 0.001 0.0005 0.0001 0.00005 0.000001;
+for lr in 1 0.5 0.1 0.05 0.01 0.005 0.001 0.0005 0.0001 0.00005 0.00001 0.000005 0.000001;
 do
-	for batch_size in 1 2 4 8 16 32 64 128 256 512 1024;
+	for batch_size in 1 2 4 8 16 32 64 128 256 512 1024 1900;
 	do
 	ginpipe configs/default_parameters.gin \
 			configs/train.gin \
