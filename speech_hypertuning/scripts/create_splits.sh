591309004fab4840d4ba72400a5c46d4e729df5d
Erik Ernst <erik_ernst@hotmail.com.ar>
2024-04-29 18:38:38 -0300

    feat: create splits script

---
+++ speech_hypertuning/scripts/create_splits.sh
@@ -0,0 +1,7 @@
+ginpipe configs/default_parameters.gin \
+			configs/create_splits.gin \
+			configs/load_dataset.gin \
+			configs/datasets/voxceleb1.gin \
+			--module_list configs/imports \
+			--project_name create_splits \
+			--experiment_name "random"
