57650b384ce37df9846987fccd9373d851bffcc1
Erik <erik.ernst@muttdata.ai>
2024-05-12 00:46:42 -0300

    feat: export DownstreamForCls

--- speech_hypertuning/tasks/models/__init__.py
+++ speech_hypertuning/tasks/models/__init__.py
@@ -1,5 +1,5 @@
 from .extract import save_upstream_embeddings
 from .fit import fit_model
 from .load import load_model
-from .models import S3PRLUpstreamMLPDownstreamForCls
+from .models import DownstreamForCls, S3PRLUpstreamMLPDownstreamForCls
 from .test import test_model
