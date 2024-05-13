55d65d96b55ce8374e899ec0c3583b95b9940d22
Erik <erik.ernst@muttdata.ai>
2024-05-13 19:59:46 -0300

    feat: test pooling

---
+++ speech_hypertuning/tests/test_pooling.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+import torch
+
+from speech_hypertuning.tasks.models.pooling import TemporalMeanPooling
+
+
+class PoolingTestCase(TestCase):
+    def setUp(self):
+        # Set random seeds
+        torch.manual_seed(44)
+        torch.use_deterministic_algorithms(True)
+
+    def test_single_embedding_1_hidden_dim_TMP_pooling(self):
+        pooling_layer = TemporalMeanPooling()
+        embedding = torch.FloatTensor([[[[1], [0]]]])
+
+        expected_out = torch.FloatTensor([[[0.5]]])
+        actual_out = pooling_layer(embedding)
+
+        self.assertEqual(actual_out, expected_out)
+
+    def test_single_embedding_2_hidden_dim_TMP_pooling(self):
+        pooling_layer = TemporalMeanPooling()
+        embedding = torch.FloatTensor([[[[1, 0], [0, 2]]]])
+
+        expected_out = torch.FloatTensor([[[0.5, 1]]])
+        actual_out = pooling_layer(embedding)
+
+        torch.testing.assert_close(actual_out, expected_out)
