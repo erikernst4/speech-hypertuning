d0287a4c3a1d4cdc800598632e2c25da4447f6cf
Erik Ernst <erik_ernst@hotmail.com.ar>
2024-05-19 18:34:46 -0300

    fix: layer-wise pooling

--- speech_hypertuning/tasks/models/pooling.py
+++ speech_hypertuning/tasks/models/pooling.py
@@ -6,15 +6,20 @@ class TemporalMeanPooling(torch.nn.Module):
     Computes Temporal Mean Pooling for each layer.
     """
 
-    def forward(self, xs: torch.Tensor):
+    def forward(self, xs: torch.Tensor, xs_len: torch.LongTensor):
         """
         Args:
             xs (torch.Tensor): Input tensor (#batch, #hidden_states, input_size, hidden_dim).
         Returns:
             torch.Tensor: Output tensor (#batch, input_size)
         """
+        pooled_list = []
+        for x_idx, x in enumerate(xs):
+            pooled_layers = []
+            for layer_idx, layer in enumerate(x):
+                cut_padding_idx = xs_len[layer_idx][x_idx]
+                pooled_layer = torch.mean(layer[:cut_padding_idx], dim=0)
+                pooled_layers.append(pooled_layer)
+            pooled_list.append(torch.stack(pooled_layers))
 
-        # Compute mean along the temporal dimension
-        mean_pooled = torch.mean(xs, dim=2)
-
-        return mean_pooled
+        return torch.stack(pooled_list)
