8bd195d63382bae440fdb8953b641364da0386d7
Erik Ernst <erik_ernst@hotmail.com.ar>
2024-06-14 19:13:12 -0300

    refactor: attentive poolings

--- speech_hypertuning/tasks/models/__init__.py
+++ speech_hypertuning/tasks/models/__init__.py
@@ -1,4 +1,17 @@
-from .extract import save_upstream_embeddings
+from .attention_poolings import SelfAttentionPooling, TransformerPooling, SummaryMixingPooling
+from .extract import (
+    save_upstream_embeddings,
+    extract_upstream_embedding_w_temporal_average_pooling,
+    extract_upstream_embedding_w_temporal_minmax_pooling,
+    extract_upstream_embedding_w_temporal_statistics_pooling,
+    extract_upstream_embedding_w_temporal_statistics_plus_pooling,
+    extract_upstream_embedding_w_temporal_max_pooling,
+    extract_upstream_embedding_w_temporal_min_pooling,
+    extract_upstream_embedding_w_std_pooling,
+    calculate_dataset_pooled_mean_and_std,
+    calculate_layerwise_dataset_pooled_mean_and_std,
+    calculate_dataset_pooled_upstream_mean_and_std_from_wavs
+)
 from .fit import fit_model
 from .load import load_model
 from .models import DownstreamForCls, S3PRLUpstreamMLPDownstreamForCls
