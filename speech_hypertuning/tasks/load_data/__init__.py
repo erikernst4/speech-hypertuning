de55d28ec2cf7ac138a2281628dce8737eeb0378
Erik Ernst <erik_ernst@hotmail.com.ar>
2024-07-07 20:02:53 -0300

    fix: typo

--- speech_hypertuning/tasks/load_data/__init__.py
+++ speech_hypertuning/tasks/load_data/__init__.py
@@ -1,19 +1,9 @@
 from .dataloaders import DictDataset
-from .load import (
-    load_dataset,
-    read_audiodir,
-    subsample_dataset,
-    subsample_dataset_with_fixed_n,
-)
-from .process import (
-    calculate_prior_distribution_entropy,
-    collate_precalculated_embeddings,
-    compensate_lengths,
-    create_splitataset_fixed_split,
-    dataset_random_split,
-    dynamic_pad_batch,
-    get_dataloaders,
-    process_classes,
-    remove_long_audios,
-)
+from .load import (load_dataset, read_audiodir, subsample_dataset,
+                   subsample_dataset_with_fixed_n)
+from .process import (calculate_prior_distribution_entropy,
+                      collate_precalculated_embeddings, compensate_lengths,
+                      create_splits, dataset_fixed_split, dataset_random_split,
+                      dynamic_pad_batch, get_dataloaders, process_classes,
+                      remove_long_audios)
 from .processor import ProcessorLoadUpstreamEmbedding, ProcessorReadAudio
