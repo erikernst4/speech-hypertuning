5597a6a56e3cfc502470d5ed7db719c1f1bc54e9
Erik Ernst <erik_ernst@hotmail.com.ar>
2024-07-07 19:54:00 -0300

    feat: self attention variants

--- speech_hypertuning/scripts/run.sh
+++
@@ -1,7 +0,0 @@
-ginpipe configs/default_parameters.gin \
-			configs/train.gin \
-			configs/load_dataset.gin \
-			configs/datasets/voxceleb1.gin \
-			--module_list configs/imports \
-			--project_name experiment_lr \
-			--experiment_name test_load_dataset
--- speech_hypertuning/scripts/self-attention-pooling.sh
+++ speech_hypertuning/scripts/self-attention-pooling.sh
@@ -9,12 +9,76 @@ ginpipe configs/default_parameters.gin \
 			    --mods "tasks.models.S3PRLUpstreamMLPDownstreamForCls.pooling_layer=@tasks.models.SelfAttentionPooling"
 
 
-# ginpipe configs/default_parameters.gin \
-# 			configs/train.gin \
-# 			configs/load_dataset.gin \
-# 			configs/load_wavs.gin \
-# 			configs/datasets/voxceleb1.gin \
-# 			--module_list configs/imports \
-# 			--project_name 11-self-attention-pooling \
-# 			--experiment_name "self_attention-1_head" \
-# 			    --mods "tasks.models.S3PRLUpstreamMLPDownstreamForCls.pooling_layer=@tasks.models.SelfAttentionPooling" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.normalize_upstream_embeddings=True" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.normalization_method='dataset_scaling'" "tasks.models.calculate_dataset_pooled_upstream_mean_and_std_from_wavs.saving_path='attn_embeddings'" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.normalize_upstream_embeddings=True" "tasks.models.calculate_dataset_pooled_upstream_mean_and_std_from_wavs.model_cls=@tasks.models.S3PRLUpstreamMLPDownstreamForCls"
+ginpipe configs/default_parameters.gin \
+			configs/train.gin \
+			configs/load_dataset.gin \
+			configs/load_wavs.gin \
+			configs/datasets/voxceleb1.gin \
+			--module_list configs/imports \
+			--project_name 11-self-attention-pooling \
+			--experiment_name "1_head-w_scaling" \
+			    --mods "tasks.models.S3PRLUpstreamMLPDownstreamForCls.pooling_layer=@tasks.models.SelfAttentionPooling" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.normalize_upstream_embeddings=True" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.normalization_method='dataset_scaling'" "tasks.models.calculate_dataset_upstream_mean_and_std_from_wavs.saving_path='/home/eernst/Voxceleb1/wavlm'" "tasks.models.calculate_dataset_upstream_mean_and_std_from_wavs.model_cls=@tasks.models.S3PRLUpstreamMLPDownstreamForCls"
+
+
+ginpipe configs/default_parameters.gin \
+			configs/train.gin \
+			configs/load_dataset.gin \
+			configs/load_wavs.gin \
+			configs/datasets/voxceleb1.gin \
+			--module_list configs/imports \
+			--project_name 11-self-attention-pooling \
+			--experiment_name "1_head-w_scaling-w_pos_encoding" \
+			    --mods "tasks.models.PositionalEncoding.dropout=0.0" "tasks.models.SelfAttentionPooling.use_positional_encoding=True" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.pooling_layer=@tasks.models.SelfAttentionPooling" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.normalize_upstream_embeddings=True" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.normalization_method='dataset_scaling'" "tasks.models.calculate_dataset_upstream_mean_and_std_from_wavs.saving_path='/home/eernst/Voxceleb1/wavlm'" "tasks.models.calculate_dataset_upstream_mean_and_std_from_wavs.model_cls=@tasks.models.S3PRLUpstreamMLPDownstreamForCls"
+
+ginpipe configs/default_parameters.gin \
+			configs/train.gin \
+			configs/load_dataset.gin \
+			configs/load_wavs.gin \
+			configs/datasets/voxceleb1.gin \
+			--module_list configs/imports \
+			--project_name 11-self-attention-pooling \
+			--experiment_name "1_head-w_scaling-w_pos_encoding_+_dropout" \
+			    --mods "tasks.models.SelfAttentionPooling.dropout=0.1" "tasks.models.SelfAttentionPooling.use_positional_encoding=True" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.pooling_layer=@tasks.models.SelfAttentionPooling" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.normalize_upstream_embeddings=True" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.normalization_method='dataset_scaling'" "tasks.models.calculate_dataset_upstream_mean_and_std_from_wavs.saving_path='/home/eernst/Voxceleb1/wavlm'" "tasks.models.calculate_dataset_upstream_mean_and_std_from_wavs.model_cls=@tasks.models.S3PRLUpstreamMLPDownstreamForCls"
+
+
+ginpipe configs/default_parameters.gin \
+			configs/train.gin \
+			configs/load_dataset.gin \
+			configs/load_wavs.gin \
+			configs/datasets/voxceleb1.gin \
+			--module_list configs/imports \
+			--project_name 11-self-attention-pooling \
+			--experiment_name "1_head-w_scaling-wo_pos_encoding-w_dropout" \
+			    --mods "tasks.models.SelfAttentionPooling.dropout=0.1" "tasks.models.SelfAttentionPooling.use_positional_encoding=False" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.pooling_layer=@tasks.models.SelfAttentionPooling" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.normalize_upstream_embeddings=True" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.normalization_method='dataset_scaling'" "tasks.models.calculate_dataset_upstream_mean_and_std_from_wavs.saving_path='/home/eernst/Voxceleb1/wavlm'" "tasks.models.calculate_dataset_upstream_mean_and_std_from_wavs.model_cls=@tasks.models.S3PRLUpstreamMLPDownstreamForCls"
+
+
+ginpipe configs/default_parameters.gin \
+			configs/train.gin \
+			configs/load_dataset.gin \
+			configs/load_wavs.gin \
+			configs/datasets/voxceleb1.gin \
+			--module_list configs/imports \
+			--project_name 11-self-attention-pooling \
+			--experiment_name "8_heads-w_scaling-w_pos_encoding_+_dropout" \
+			    --mods "tasks.models.SelfAttentionPooling.num_heads=8" "tasks.models.SelfAttentionPooling.dropout=0.1" "tasks.models.SelfAttentionPooling.use_positional_encoding=True" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.pooling_layer=@tasks.models.SelfAttentionPooling" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.normalize_upstream_embeddings=True" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.normalization_method='dataset_scaling'" "tasks.models.calculate_dataset_upstream_mean_and_std_from_wavs.saving_path='/home/eernst/Voxceleb1/wavlm'" "tasks.models.calculate_dataset_upstream_mean_and_std_from_wavs.model_cls=@tasks.models.S3PRLUpstreamMLPDownstreamForCls"
+
+ginpipe configs/default_parameters.gin \
+			configs/train.gin \
+			configs/load_dataset.gin \
+			configs/load_wavs.gin \
+			configs/datasets/voxceleb1.gin \
+			--module_list configs/imports \
+			--project_name 11-self-attention-pooling \
+			--experiment_name "16_heads-w_scaling-w_pos_encoding_+_dropout" \
+			    --mods "tasks.models.SelfAttentionPooling.num_heads=16" "tasks.models.SelfAttentionPooling.dropout=0.1" "tasks.models.SelfAttentionPooling.use_positional_encoding=True" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.pooling_layer=@tasks.models.SelfAttentionPooling" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.normalize_upstream_embeddings=True" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.normalization_method='dataset_scaling'" "tasks.models.calculate_dataset_upstream_mean_and_std_from_wavs.saving_path='/home/eernst/Voxceleb1/wavlm'" "tasks.models.calculate_dataset_upstream_mean_and_std_from_wavs.model_cls=@tasks.models.S3PRLUpstreamMLPDownstreamForCls"
+
+
+ginpipe configs/default_parameters.gin \
+			configs/train.gin \
+			configs/load_dataset.gin \
+			configs/load_wavs.gin \
+			configs/datasets/voxceleb1.gin \
+			--module_list configs/imports \
+			--project_name 11-self-attention-pooling \
+			--experiment_name "32_heads-w_scaling-w_pos_encoding_+_dropout" \
+			    --mods "tasks.models.SelfAttentionPooling.num_heads=32" "tasks.models.SelfAttentionPooling.dropout=0.1" "tasks.models.SelfAttentionPooling.use_positional_encoding=True" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.pooling_layer=@tasks.models.SelfAttentionPooling" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.normalize_upstream_embeddings=True" "tasks.models.S3PRLUpstreamMLPDownstreamForCls.normalization_method='dataset_scaling'" "tasks.models.calculate_dataset_upstream_mean_and_std_from_wavs.saving_path='/home/eernst/Voxceleb1/wavlm'" "tasks.models.calculate_dataset_upstream_mean_and_std_from_wavs.model_cls=@tasks.models.S3PRLUpstreamMLPDownstreamForCls"
