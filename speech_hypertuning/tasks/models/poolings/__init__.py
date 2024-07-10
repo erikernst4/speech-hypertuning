6d8d580e1385cfe26cd6e9a3dd5d3010f3d50ccb
Erik Ernst <erik_ernst@hotmail.com.ar>
2024-07-10 19:17:37 -0300

    fix: TemporalMeanPooling location

--- speech_hypertuning/tasks/models/poolings/__init__.py
+++ speech_hypertuning/tasks/models/poolings/__init__.py
@@ -1,5 +1,11 @@
-from .layer_pooling import (FixedLayerPooling, SelfAttentionLayerPooling,
-                            WeightedAverageLayerPooling)
-from .mean_pooling import TemporalMeanPooling
-from .time_pooling import (SelfAttentionPooling, SummaryMixingPooling,
-                           TransformerPooling)
+from .layer_pooling import (
+    FixedLayerPooling,
+    SelfAttentionLayerPooling,
+    WeightedAverageLayerPooling,
+)
+from .time_pooling import (
+    SelfAttentionPooling,
+    SummaryMixingPooling,
+    TransformerPooling,
+    TemporalMeanPooling,
+)
